500b3d0872d62af597db5db5582c47cb3c2ff735
Your Name <nong0005@algonquinlive.com>
2025-12-01 18:10:04 -0500

    Update logo and assets for production

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,8 @@
 // src/components/Logo.jsx
 import React from "react";
-import logo from "../assets/logo/logo.png";
 
 const Logo = ({ className }) => {
-  return <img src={logo} alt="Logo" className={className || "h-10 w-auto"} />;
+  return <img src="/logo/logo.png" alt="Logo" className={className || "h-10 w-auto"} />;
 };
 
 export default Logo;

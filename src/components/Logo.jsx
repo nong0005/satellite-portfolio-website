9b95c04ef04c99b67f02f6bec84ba416657b6f30
Your Name <nong0005@algonquinlive.com>
2025-12-01 20:19:21 -0500

    logo update

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 
 const Logo = ({ className }) => {
-  return <img src="/logo/logo.png" alt="Logo" className={className || "h-10 w-auto"} />;
+  return <img src="public/logo/logo.png" alt="Logo" className={className || "h-10 w-auto"} />;
 };
 
 export default Logo;
